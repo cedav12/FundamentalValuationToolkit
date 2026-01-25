79504c9827fe7d82a482e806448ce356c4b3f966
Simon <85981407@fsv.cuni.cz>
2026-01-25 15:34:56 +0100

    Update main.py
    descriptions

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import argparse
 from data.pipeline import AnalysisPipeline
 
 parser = argparse.ArgumentParser()
-
+# General settings
 parser.add_argument("--tickers", nargs="+", type=str, required=True, help="list of tickers to be analyzed")
 parser.add_argument("--output_path", default="output_reports", type=str,
                     help="Specify path, where output will be saved. If None, no output will be saved")
 parser.add_argument( "--show_plt", action="store_true",
                      help="If set, display plots interactively. Otherwise, just save to output_path.")
+# Overrides
 parser.add_argument("--growth", type=float, help="Override revenue growth")
 parser.add_argument("--margin", type=float, help="Override target operating margin")
 parser.add_argument("--wacc", type=float, help="Override WACC")
@@ -21,13 +22,13 @@ def main(args: argparse.Namespace):
         "terminal_growth": args.terminal_growth        
     }
     overrides = {k: v for k, v in overrides.items() if v is not None}
-
+# Initialize and execute pipeline
     pipeline = AnalysisPipeline(args.output_path, args.show_plt, overrides=overrides)
     pipeline.run(args.tickers)
 
 
 if __name__ == "__main__":
-    # Parse arguments
+    # Parse arguments and run
     arg = parser.parse_args([] if "__file__" not in globals() else None)
     main(arg)
 
